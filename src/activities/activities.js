bdc71bf4e4e89be77598cd3ac00fd042d942ef31
mpopova <urgo@abv.bg>
2017-09-30 22:02:32 +0300

    Adding state for clicking on any sport

--- src/activities/activities.js
+++ src/activities/activities.js
@@ -3,12 +3,22 @@ import { Router, Route, Link, browserHistory, IndexRoute  } from 'react-router'
 
 
 class Activities extends React.Component {
+	constructor(props) {
+	    super(props);
+	    this.state = {text: 'Click on any sport to see info about it :)'}
+	    this.onClick = this.onClick.bind(this);
+	}
+	onClick(e) {
+    	this.setState({
+	      text: 'OTHER STATE',
+	    });
+  	}
+
   render() {
     return (
     <div>
-    	<div><Link to="activities">Football</Link></div>
-      		<h1>Click on any sport to see info about it :) </h1>
-      	<div><Link to="activities">Tenis</Link></div>
+      		<h1>{this.state.text}</h1>
+      		<div onClick={this.onClick}>Tenis</div>
    	 </div>
     )
   }
